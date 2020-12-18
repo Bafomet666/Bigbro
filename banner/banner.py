5d66a98a759e88029ca7f1c3c69e0f2599842984
Maxx <72593411+Bafomet666@users.noreply.github.com>
2020-12-18 15:26:57 +0000

    Add files via upload

---
+++ banner/banner.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#Developer by Bafomet
+import time
+import sys
+# Set color
+R = '\033[31m'   # Red
+N = '\033[1;37m' # White
+G = '\033[32m'   # Green
+O = '\033[0;33m' # Orange
+B = '\033[1;34m' # Blue
+
+def delay_print(s):
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.001)
+delay_print
+delay_print (""+B+"                                                                                                                  \n")
+delay_print (""+B+"                                                                                                                  \n")
+delay_print (""+B+" ______         ______                 "+B+" db      db  "+G+" Dev Bafomet                                        \n")
+delay_print (""+B+"| ___ (_)       | ___ \                "+R+"  db    db   "+G+" Github "+R+" https://github.com/Bafomet666         \n")
+delay_print (""+B+"| |_/ /_  __ _  | |_/ /_ __ ___        "+R+"  `8b  d8    "+G+" Made for educational purposes only                 \n")
+delay_print (""+B+"| ___ \ |/ _` | | ___ \ '__/ _ \       "+R+"   `8bd8'    "+G+" Use only as a demonstration.                       \n")
+delay_print (""+B+"| |_/ / | (_| | | |_/ / | | (_) |      "+R+"   .dPYb.    "+G+" Telegram "+R+" https://t.me/hacknocrime            \n")
+delay_print (""+B+"\____/|_|\__, | \____/|_|  \___/       "+R+"  .8P  Y8.   "+G+" Help development develop exploit trail             \n")
+delay_print (""+B+"          __/ |                        "+R+"  db    db   "+G+" Для получения премиум версии писать :              \n")
+delay_print (""+B+"          |___/                        "+B+" db      db  "+R+" https://t.me/satana666mx                           \n")
+print                                                    
+                                                     
