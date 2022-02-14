734b93091e978d52760628d59ac096dcbcfc3494
Максим <72593411+Bafomet666@users.noreply.github.com>
2022-02-14 01:59:30 +0000

    Update api.py

--- api.py
+++ api.py
@@ -3,6 +3,6 @@
 #  Получать токен в -->> https://dashboard.ngrok.com/get-started/your-authtoken
 
 api = {
-    "ngrok.set_auth_token": "D54fufNfLVcaW88E5F2X_2Ld915BBkoWKM1DG7qMaV",
+    "ngrok.set_auth_token": "D54fufNfLVcaW88E5F24857ytgihgj3dg1DG7qMaV",
     # big brother 13,  "сюда_вписывать_ваш_токен"
 }
