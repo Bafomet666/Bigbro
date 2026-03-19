073171b1e6ed5722844ba15878d5d0071be40bf4
Максим Понамарев <72593411+Bafomet666@users.noreply.github.com>
2026-03-19 17:18:04 +0300

    Delete api.py

--- api.py
+++
@@ -1,8 +0,0 @@
-
-#  Привет, добавьте API пожалуйста
-#  Получать токен в -->> https://dashboard.ngrok.com/get-started/your-authtoken
-
-api = {
-    "ngrok.set_auth_token": "D54fufNfLVcaW88E5F24857ytgihgj3dg1DG7qMaV",
-    # big brother 13,  "сюда_вписывать_ваш_токен"
-}
